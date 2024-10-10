705498a7f3c015bfc9ae1c533df38ee7da799613
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-10-10 14:21:08 +0600

    Added the fetchData for fetching the jokes from url, and useState to tracking and updating it. Added styles.

--- src/Containers/Jokes/Jokes.tsx
+++ src/Containers/Jokes/Jokes.tsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
+
 const Jokes = () => {
+  const url = 'https://api.chucknorris.io/jokes/random';
+  const [joke, setJoke] = useState<string>('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await fetch(url);
+      const data = await response.json();
+      setJoke(data.value);
+      console.log(response)
+    };
+
+    void fetchData();
+  }, []);
+
   return (
-    <div>
-    Jokes
+    <div className="main-container">
+      <div className='joke-container'>{joke}</div>
     </div>
   );
 };
 
 export default Jokes;
-
